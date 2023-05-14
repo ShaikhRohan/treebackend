5f3a85589c525830e6312fc402c5fb8931fa546c
RohanShaikhh <soc.solutions.rohaan@outlook.com>
2023-05-14 20:23:48 +0500

    add searching product

--- index.js
+++ index.js
@@ -1001,6 +1001,28 @@ app.post('/getf3price', async (req, res) => {
  });
 
 
+ app.post('/serachProducts', async (req, res) => {
+  const {productName , seller} = req.body;
+  try {
+    // Find user in users collection
+    const products = await Product.find({ seller , productname : { $regex: productName, $options: 'i' } });
+
+    if (products) {
+      // Create and sign a JWT token
+      return res.status(200).send(products);
+      // Return the token to the client
+    } else {
+      // Return an error message if the login fails
+      return res.status(401).send('No Product Available' );
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+
+ });
+
+
   app.listen(5000, () => {
   console.log('Server started on port 5000');
 });
